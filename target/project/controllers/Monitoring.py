ac3bdaab3bd5d134b046f4894842866b58a31f0b
asurwade <41955714+asurwade@users.noreply.github.com>
2019-08-02 13:38:07 +0200

    Update Monitoring.py

--- target/project/controllers/Monitoring.py
+++ target/project/controllers/Monitoring.py
@@ -3,9 +3,11 @@ from project.models.Messaging import *
 from flask import render_template, request, redirect, session, make_response
 import os
 
-@app.route("/monitoring", methods=['GET'])
 def monitor():
-    return render_template("monitoring/index.html")
+    if not session.get('loggedin'):
+        return redirect("/dashboard/1", code=302)
+    else:
+        return render_template("monitoring/index.html")
 
 @app.context_processor
 def utility_processor():
