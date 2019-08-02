957ca58d1b9e8185dccbea3e5359be8e14f9968b
asurwade <41955714+asurwade@users.noreply.github.com>
2019-08-02 13:59:29 +0200

    Update Monitoring.py

--- target/project/controllers/Monitoring.py
+++ target/project/controllers/Monitoring.py
@@ -3,6 +3,7 @@ from project.models.Messaging import *
 from flask import render_template, request, redirect, session, make_response
 import os
 
+@app.route("/monitoring", methods=['GET'])
 def monitor():
     if not session.get('loggedin'):
         return redirect("/dashboard/1", code=302)
